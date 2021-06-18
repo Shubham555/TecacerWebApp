59f22b120a3da35e9cb6561b4275512d6ab27d53
amit-t1234 <amitthakurashwani@gmail.com>
2021-06-19 00:58:07 +0530

    alot of changes

--- assets/js/teachermain.js
+++ assets/js/teachermain.js
@@ -41,5 +41,104 @@ function toggleSidebar() {
 $(function() {
     $('#date-carousel').carousel({
         interval: false
-    })
+    });
+
+    $("#date-carousel").swipe({
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            if (direction == 'left') $(this).carousel('next');
+            if (direction == 'right') $(this).carousel('prev');
+        },
+        allowPageScroll: "vertical" 
+    });
+
+    $(document).on('click', '#addSubjectDone', function() {
+        const subject = $('#addSubjectInput').val();
+        console.log('yes', subject);
+        if (subject && subject.length) {
+            $('.subject-group > div').append(`
+                <label class="divselect">
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                    <span class="alphabet">${subject}</span>
+                </label>
+            `);   
+        }
+        $('#addSubjectInput').val('');
+    });
+
+    $(document).on('click', '#button-addon2', function() {
+        $('#addSubjectInput').val('');
+    });
+
+    $( "#date" ).datepicker({changeMonth: true, changeYear: true, dateFormat: 'mm/dd/yy', maxDate: '+1Y', yearRange: "-100:+0",});
+    $('#starttime').timepicker({});
+    $('#endtime').timepicker({});
+
+    function moveSlider(selected) {
+        const offsetleft = selected.parent().offset().left;
+        const width = selected.parent().width();
+        console.log(offsetleft, width, selected);
+        selected.closest('.nav-tabs').find('.slider .bar').width(width);
+        selected.closest('.nav-tabs').find('.slider .bar').offset({left: offsetleft});
+    }
+
+    $('.tab a.nav-link.active').each(function() {
+        moveSlider($(this));
+    });
+
+    $('.tab a.nav-link').click(function() {
+        moveSlider($(this));
+    });
+
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $('#overlay').fadeOut('slow');
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    $(".menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+        $('#overlay').fadeToggle( "slow", "swing" );
+    });
+    $(".subject-toggle").click(function(e) {
+        e.preventDefault();
+        $("#subject-wrapper").toggleClass("active");
+    });
+    $('#overlay').click(function() {
+        $('#overlay').fadeOut('slow');
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+});
+
+// Tooltip
+
+$('button').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+});
+
+function setTooltip(btn, message) {
+    $(btn).tooltip('hide')
+        .attr('data-original-title', message)
+        .tooltip('show');
+}
+
+function hideTooltip(btn) {
+    setTimeout(function() {
+        $(btn).tooltip('hide');
+    }, 1000);
+}
+
+// Clipboard
+
+var clipboard = new Clipboard('button.js-copy-trigger');
+
+clipboard.on('success', function(e) {
+    setTooltip(e.trigger, 'Copied!');
+    hideTooltip(e.trigger);
+});
+
+clipboard.on('error', function(e) {
+    setTooltip(e.trigger, 'Failed!');
+    hideTooltip(e.trigger);
 });
